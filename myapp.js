9ae77e724d56d7264f04f275fcb525724bc5c705
bsand <benasand@gmail.com>
2018-05-24 12:02:56 -0400

    cleaned up files and added key tracking to let users enter lines into the field

--- myapp.js
+++ myapp.js
@@ -3,16 +3,11 @@
 /*import good from './JSExFiles'*/
 import Promise from 'bluebird';
 
-let centurionEl, consoleEl;
+let centurionEl, consoleEl, inputEl;
 let promptMSg = "Welcome Centurion. You've demonstrated yourself to be clever enough to uncover the first mystery. Would you like to go deeper?"
-window.onload = function()
-{
+var endOfLine= require('os').EOL;
 
-    /*document.getElementById("test").innerHTML = "does it have to be under";*/
-}
-/*
-let testmessage = "okay, lets see how this works."
-*/
+let gameStart = false;
 
 document.addEventListener("DOMContentLoaded", function() {
 
@@ -39,15 +34,23 @@ async function writeAnimate(msg, element)
     element.innerHTML = ''
   }
   console.log(msg.length)
-  speed = 120
+  speed = 50
   await Promise.delay(speed*3)
   for (var index = 0; index < msg.length; index++) {
-      chars = msg.slice(index,index+1)
-      writeMsg(chars, element)
+    chars = msg.slice(index,index+1)
+    writeMsg(chars, element)
 
-      await Promise.delay(speed)
+    await Promise.delay(speed)
 
   }
+  enableInput();
+}
+
+function enableInput()
+{
+
+  inputEl.innerHTML = "hrmmm yess ";
+
 
 }
 
@@ -55,25 +58,42 @@ function  getControlEls()
 {
   centurionEl = document.getElementById("centurionDiv");
   consoleEl = document.getElementById("work-text")
+  inputEl = document.getElementById('user-input');
 }
 function createEventHandlers()
 {
   console.log("creating Event Handlers");
   centurionEl.addEventListener("click", function() {
-    writeAnimate(promptMSg, consoleEl);
+    if (!gameStart) {
+      writeAnimate(promptMSg, consoleEl);
+      gameStart=true;
+    }
   });
   centurionEl.addEventListener("mouseover", simpleLog("over"));
   centurionEl.addEventListener("mouseout", function() {
     simpleLog("out");
-  }
-)
-
+  });
+  inputEl.addEventListener("keydown", function(e) {
+    getUserInput(e);
+  });
 }
+
+function getUserInput(e) {
+  console.log(e.key);
+  let userMsg;
+  if(e.key === 'Enter') {
+    userMsg = inputEl.innerHTML;
+    inputEl.innerHTML = " ";
+    consoleEl.innerHTML += "------------------\n";
+    consoleEl.innerHTML += userMsg;
+  }
+}
+
 function simpleLog (callerType){
   console.log("simpleLog" + callerType);
 }
 
 function writeMsg(msg, element) {
-    element.innerHTML += msg
+  element.innerHTML += msg
 
 }
